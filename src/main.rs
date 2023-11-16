f7be756d65829690337cf5ef5e89f61195a105e5
Romeo Disca <romeo.disca@gmail.com>
2023-11-16 18:51:24 +0100

    blink created

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,28 @@
-fn main() {
-    // It is necessary to call this function once. Otherwise some patches to the runtime
-    // implemented by esp-idf-sys might not link properly. See https://github.com/esp-rs/esp-idf-template/issues/71
-    esp_idf_svc::sys::link_patches();
+use std::thread;
+use std::time::Duration;
+use esp_idf_hal::gpio::*;
+use esp_idf_hal::peripherals::Peripherals;
 
-    // Bind the log crate to the ESP Logging facilities
-    esp_idf_svc::log::EspLogger::initialize_default();
+fn main() 
+{
+  esp_idf_sys::link_patches();
 
-    log::info!("Hello, world!");
+  let mut led = builtin_led();
+  let n = 1;
+
+  while n == 1 {
+    led.set_high().unwrap();
+    thread::sleep(Duration::from_millis(100));
+
+    led.set_low().unwrap();
+    thread::sleep(Duration::from_millis(800));
+
+    println!("blink");
+  }
+}
+
+fn builtin_led() -> PinDriver<'static, Gpio2, Output>
+{
+  let peripherals = Peripherals::take().unwrap();
+  PinDriver::output(peripherals.pins.gpio2).unwrap()
 }
